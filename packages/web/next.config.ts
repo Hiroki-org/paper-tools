61926f31fc63eb83d787eb11ebf34b74b9a0386c
is0692vs <is0692vs@ed.ritsumei.ac.jp>
2026-03-15 18:39:19 +0900

    fix(web): harden setup icon image handling

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -5,7 +5,23 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "**",
+                hostname: "www.notion.so",
+            },
+            {
+                protocol: "https",
+                hostname: "notion.so",
+            },
+            {
+                protocol: "https",
+                hostname: "secure.notion-static.com",
+            },
+            {
+                protocol: "https",
+                hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+            },
+            {
+                protocol: "https",
+                hostname: "s3.us-west-2.amazonaws.com",
             },
         ],
     },
