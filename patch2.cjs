65eceae1f4ce866ae75f4327bc48641f651be17e
is0692vs <is0692vs@ed.ritsumei.ac.jp>
2026-03-31 01:04:45 +0900

    chore: drop temporary patch scripts

--- patch2.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const filePath = 'packages/author-profiler/src/tests/author-client.test.ts';
-let code = fs.readFileSync(filePath, 'utf8');
-code = code.replace(
-    'import { describe, it, expect, vi, beforeEach } from "vitest";',
-    'import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";'
-);
-fs.writeFileSync(filePath, code);
